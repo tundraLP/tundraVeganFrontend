2fa6bcffefed401b35b8aac2d93823c4dcc9fde9
BlasCasale <casale.blas@live.com>
2023-12-20 19:32:23 -0300

    arreglando la store de redux

--- main.jsx
+++ main.jsx
@@ -1,11 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import App from './src/App';
 import { Provider } from 'react-redux';
+import { store } from './src/redux/store'
 
 const root = createRoot(document.getElementById("app"));
 
 root.render(
-    <Provider>
+    <Provider store={store}>
         <App />
     </Provider>
 );
---
+++ src/redux/actions.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export const actions = {
+    SIGN_IN: 'SIGN_IN',
+    SIGN_OUT: 'SIGN_OUT'
+};
